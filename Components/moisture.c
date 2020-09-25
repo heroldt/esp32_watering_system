ae6617b218511d8cda760f87b1ab552a914f5623
Thomas Herold <thomasherold@outlook.com>
2020-09-26 00:43:27 +0200

    added adc driver

--- Components/moisture.c
+++ Components/moisture.c
@@ -1,11 +1,20 @@
 #include "moisture.h"
 
-moisture_error_t moisture_get_raw_value(uint8_t * value)
+moisture_error_t moisture_get_raw_value(const adc1_channel_t channel, uint16_t * value)
 {
+	adc1_config_width(ADC_WIDTH_BIT_12);
+	adc1_config_channel_atten(channel,ADC_ATTEN_DB_0);
+	*value = adc1_get_raw(channel);
+	if(*value < 0)
+	{
+		*value = 0;
+		return MOISTURE_ERROR_ADC;
+	}
+
 	return MOISTURE_ERROR_OK;
 }
 
-moisture_error_t moisture_get_percent(uint8_t * value)
+moisture_error_t moisture_get_percent(uint16_t * value)
 {
 	return MOISTURE_ERROR_OK;
 }
--- Components/moisture.h
+++ Components/moisture.h
@@ -9,14 +9,16 @@
 #define MOISTURE_H_
 
 #include <stdint.h>
+#include <driver/adc.h>
 
 typedef enum{
-	MOISTURE_ERROR_OK,
+	MOISTURE_ERROR_OK = 0,
+	MOISTURE_ERROR_ADC,
 	MOISTURE_ERROR_UKNOWN
 }moisture_error_t;
 
-moisture_error_t moisture_get_raw_value(uint8_t * value);
+moisture_error_t moisture_get_raw_value(const adc1_channel_t channel, uint16_t * value);
 
-moisture_error_t moisture_get_percent(uint8_t * value);
+moisture_error_t moisture_get_percent(uint16_t * value);
 
 #endif /* MOISTURE_H_ */
