3833566e77d17a746f50244f9c5b7af6ae6636f0
Thomas Herold <thomasherold@outlook.com>
2020-09-26 11:26:50 +0200

    fixed moisture reading, added debug output

--- Components/moisture.c
+++ Components/moisture.c
@@ -1,15 +1,22 @@
 #include "moisture.h"
+#include "esp_log.h"
+
+static const char* TAG = "Moisture";
 
 moisture_error_t moisture_get_raw_value(const adc1_channel_t channel, uint16_t * value)
 {
+	int adc_raw_val = 0;
+
 	adc1_config_width(ADC_WIDTH_BIT_12);
-	adc1_config_channel_atten(channel,ADC_ATTEN_DB_0);
-	*value = adc1_get_raw(channel);
-	if(*value < 0)
+	adc1_config_channel_atten(channel,ADC_ATTEN_DB_11);
+	adc_raw_val = adc1_get_raw(channel);
+	if(adc_raw_val < 0)
 	{
-		*value = 0;
+		ESP_LOGE(TAG,"ADC channel %d read failed.",channel);
 		return MOISTURE_ERROR_ADC;
 	}
+	ESP_LOGI(TAG,"ADC channel %d raw value %u",channel,adc_raw_val);
+	*value = (uint16_t)adc_raw_val;
 
 	return MOISTURE_ERROR_OK;
 }
